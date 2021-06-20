8347b9715065311be93d6b2860726e34d113b870
Maksim Ivanov <ivanovmaximant@gmail.com>
2021-06-20 11:50:17 +0300

    Wrap the UK VAT number API call into own API

--- check_vat.py
+++ check_vat.py
@@ -14,11 +14,15 @@ def home():
            '</form>'
 
 @app.route('/', methods=['POST'])
-def check_vat():
+def vat_input_redirect():
     vat = flask.request.form['vat']
+    return flask.redirect('/fiscal-number-information/GB/' + vat)
+
+@app.route('/fiscal-number-information/GB/<vat_id>')
+def check_vat(vat_id):
     base_url = 'https://api.service.hmrc.gov.uk'
     lookup_path = '/organisations/vat/check-vat-number/lookup/'
-    response = requests.get(base_url + lookup_path + vat)
+    response = requests.get(base_url + lookup_path + vat_id)
     response_json = response.json()
 
     result_dict = dict()
@@ -26,8 +30,12 @@ def check_vat():
         if 'code' in response_json:
             if response_json['code'] == 'INVALID_REQUEST':
                 result_dict['valid'] = 'false'
-                result_dict['errorMessage'] = 'Enter the UK VAT number you want '\
-                                              'to check in the correct format'
+                result_dict['errorMessage'] = 'Enter the UK VAT number you '\
+                                              'want to check in the '\
+                                              'correct format'
+            else:
+                result_dict['valid'] = 'false'
+                result_dict['errorMessage'] = response_json['message']
         elif 'target' in response_json:
             result_dict['valid'] = 'true'
             result_dict['businessName'] = response_json['target']['name']
